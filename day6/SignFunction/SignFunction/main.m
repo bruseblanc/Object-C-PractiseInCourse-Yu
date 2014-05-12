0a9a9f2ff743230a9cf6be868a0c3ea3325e1e2f
bruseblanc <user1@Administrators-iMac-68.local>
2014-05-12 13:58:12 -0400

    Initial Commit

---
+++ day6/SignFunction/SignFunction/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SignFunction
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 lambton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
--- day6/caculateLeapYear/caculateLeapYear/main.m
+++ day6/caculateLeapYear/caculateLeapYear/main.m
@@ -12,10 +12,20 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        int year,rem_4,rem_100,rem_400;
+        NSLog(@"Enter the year to be test:");
+        scanf("%i",&year);
+        rem_4=year%4;
+        rem_100=year%100;
+        rem_400=year%400;
+        if((rem_4==0 && rem_100!=0) || rem_400==0)
+        {
+            NSLog(@"It is a leap year");
+            
+        }else
+        {
+            NSLog(@"No it's not a leap year");
+        }
     }
     return 0;
 }
