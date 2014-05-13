31e95b7e3fb09a972508bc25ae7b10bf3a33d019
bruseblanc <user1@Administrators-iMac-68.local>
2014-05-13 13:59:59 -0400

    Initial Commit

--- day6/Day7/Calculator/Calculator/main.m
+++ day6/Day7/Calculator/Calculator/main.m
@@ -7,14 +7,75 @@
 //
 
 #import <Foundation/Foundation.h>
+@interface Calculator:NSObject
+-(void)setAccumulator:(double)value;
+-(void)clear;
+-(double)caccumulator;
+//aritimetic methodes
+-(void) add:(double)value;
+-(void) subtract:(double)value;
+-(void) multiply:(double)value;
+-(void) divide:(double)value;
+
+@end
+
+
+@implementation Calculator
+
+{
+    double accumulator;
+}
+ -(void)setAccumulator:(double)value
+{
+    accumulator=value;
+}
+-(void)clear
+{
+    accumulator=0;
+}
+-(double)caccumulator
+{
+    return accumulator;
+}
+//
+
+-(void)add:(double)value
+{
+    accumulator+=value;
+}
+-(void)subtract:(double)value{
+    accumulator-=value;
+}
+-(void)multiply:(double)value{
+    accumulator*=value;
+}
+-(void)divide:(double)value{
+    accumulator/=value;
+}
+@end
+
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        double value1,value2;
+        char operator;
+        Calculator *deskCal=[[Calculator alloc] init];
+        NSLog(@"Type in your expression");
+        scanf("%lf %c %lf",&value1,&operator,&value2);
+        [deskCal setAccumulator:value1];
+        if (operator=='+') {
+            [deskCal add:value2];
+        } else if(operator=='-' ){
+            [deskCal subtract:value2]   ;
+        }else if (operator=='*'){
+            [deskCal multiply:value2];
+        }else if(operator=='/'){
+            [deskCal divide:value2];
+        }
+        NSLog(@"%.2f",[deskCal caccumulator]    );
         
     }
     return 0;
---
+++ day6/Day7/prime/prime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prime
+//
+//  Created by User1 on 2014-05-13.
+//  Copyright (c) 2014 lambton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
